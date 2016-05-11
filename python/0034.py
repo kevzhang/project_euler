8f170bac44c37c7f31a510df1061112ab61cbd17
Kevin Zhang <kzhang@palantir.com>
2016-05-11 19:10:39 -0400

    Problem 34 : Python

---
+++ python/0034.py
@@ -0,0 +1,14 @@
+import math
+
+factorials = [math.factorial(k) for k in range(0, 10)]
+limit = 26 * 100 * 1000
+def fac_sum(num):
+	return sum(factorials[ord(digit_str) - ord("0")] for digit_str in str(num))
+
+total = 0
+for num in range(10, limit):
+	if fac_sum(num) == num:
+		total += num
+		print num
+
+print total
