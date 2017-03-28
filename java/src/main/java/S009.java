a67d8941c6971beb6492568eb90da7690868d689
Kevin Zhang <kevkzhang@gmail.com>
2017-03-28 12:43:25 -0700

    Problem 9 : Java

---
+++ java/src/main/java/S009.java
@@ -0,0 +1,22 @@
+import java.math.RoundingMode;
+
+import com.google.common.math.DoubleMath;
+
+public class S009 {
+
+    private static final int SUM = 1000;
+
+    public static long solve() {
+        int smallestC = DoubleMath.roundToInt(SUM * (Math.sqrt(2) / (Math.sqrt(2) + 2)), RoundingMode.DOWN);
+        for (int c = smallestC; c < SUM; c++) {
+            int left = SUM - c;
+            for (int a = 1; a < left; a++) {
+                int b = left - a;
+                if (a * a + b * b == c * c) {
+                    return a * b * c;
+                }
+            }
+        }
+        return -1;
+    }
+}
