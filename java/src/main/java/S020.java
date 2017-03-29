72b2a63d8f623efef49a50f79c579ed40f543a90
Kevin Zhang <kevkzhang@gmail.com>
2017-03-29 16:19:04 -0700

    Problem 20 : Java

---
+++ java/src/main/java/S020.java
@@ -0,0 +1,21 @@
+import java.math.BigInteger;
+
+import com.google.common.collect.Lists;
+
+public class S020 {
+
+    private static final int N = 100;
+    public static int solve() {
+        BigInteger fact = factorial(N);
+        return Lists.charactersOf(fact.toString()).stream()
+            .mapToInt(c -> (c - '0')).sum();
+    }
+
+    private static BigInteger factorial(int n) {
+        BigInteger fact = BigInteger.valueOf(n);
+        for (int i = n - 1; i > 1; i--) {
+            fact = fact.multiply(BigInteger.valueOf(i));
+        }
+        return fact;
+    }
+}
