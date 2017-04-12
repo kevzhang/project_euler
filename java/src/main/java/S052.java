8699f6a79bec4a415d45ff19179ec89d0e1c61da
Kevin Zhang <kevkzhang@gmail.com>
2017-04-12 16:13:24 -0700

    Problem 52 : Java

---
+++ java/src/main/java/S052.java
@@ -0,0 +1,26 @@
+import java.util.Arrays;
+
+// TODO: way too bespoke
+public class S052 {
+
+    public static int solve() {
+        for (int i = 1; i < Integer.MAX_VALUE; i++) {
+            if (isSpecial(i)) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    private static boolean isSpecial(int n) {
+        int[] digitFrequency = Maths.getDigitFrequency(n);
+        for (int multiple = 2; multiple <= 6; multiple++) {
+            if (!Arrays.equals(digitFrequency, Maths.getDigitFrequency(n * multiple))) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+}
+
