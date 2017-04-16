28c3c3ea925bbc3b47620c66c4db7158d1efe6fd
Kevin Zhang <kevkzhang@gmail.com>
2017-04-16 16:14:05 -0700

    Problem 60 : Java (optimization)

--- java/src/main/java/S060.java
+++ java/src/main/java/S060.java
@@ -11,24 +11,28 @@ public class S060 {
 
     public static long solve() {
         ArrayList<Integer> primeList = Maths.generatePrimesBelow(LIMIT);
+        // 1 -> true, 0 -> n/a, -1 -> false
+        byte[][] concatTable = new byte[primeList.size()][primeList.size()];
         int bestSum = Integer.MAX_VALUE;
-        int[] candidates = new int[GROUP_SIZE];
-        return search(bestSum, primeList, 0, candidates, 0);
+        int[] candidateIdx = new int[GROUP_SIZE], candidateValues = new int[GROUP_SIZE];
+        return search(bestSum, concatTable, primeList, 0, candidateIdx, candidateValues, 0);
     }
 
-    private static int search(int sumLimit, ArrayList<Integer> primes, int startSearchIdx, int[] candidates, int size) {
+    private static int search(int sumLimit, byte[][] concatTable, ArrayList<Integer> primes, int startSearchIdx,
+                              int[] candidateIdx, int[] candidateValues, int size) {
         if (size == GROUP_SIZE) {
-            return Maths.sum(candidates);
+            return Maths.sum(candidateValues);
         }
         int bestSum = sumLimit;
         for (int searchIdx = startSearchIdx; searchIdx < primes.size(); searchIdx++) {
-            int searchingPrime = primes.get(searchIdx).intValue();
-            candidates[size] = searchingPrime;
-            if (isTooHigh(bestSum, candidates, size + 1)) {
+            candidateIdx[size] = searchIdx;
+            candidateValues[size] = primes.get(searchIdx);
+            if (isTooHigh(bestSum, candidateValues, size + 1)) {
                 break;
             }
-            if (isLastItemValidWithRest(candidates, size + 1)) {
-                bestSum = Math.min(bestSum, search(bestSum, primes, searchIdx + 1, candidates, size + 1));
+            if (isLastItemValidWithRest(primes, concatTable, candidateIdx, size + 1)) {
+                bestSum = Math.min(bestSum, search(bestSum, concatTable, primes, searchIdx + 1,
+                        candidateIdx, candidateValues, size + 1));
             }
         }
         return bestSum;
@@ -39,15 +43,28 @@ public class S060 {
         return (GROUP_SIZE - size) * candidates[size - 1] + curSum > sumLimit;
     }
 
-    private static boolean isLastItemValidWithRest(int[] candidates, int limit) {
-        int last = candidates[limit - 1];
+    private static boolean isLastItemValidWithRest(ArrayList<Integer> primes, byte[][] concatTable,
+                                                   int[] candidateIdx, int limit) {
+        int last = candidateIdx[limit - 1];
         for (int i = limit - 2; i >= 0; i--) {
-            int compareTo = candidates[i];
-            if (!IntMath.isPrime(concat(last, compareTo)) || !IntMath.isPrime(concat(compareTo, last))) {
+            int compareTo = candidateIdx[i];
+            if (!concatsToPrimeCached(concatTable, primes, compareTo, last)
+                    || !concatsToPrimeCached(concatTable, primes, last, compareTo)) {
                 return false;
             }
         }
         return true;
+    }
+
+    private static boolean concatsToPrimeCached(byte[][] table, ArrayList<Integer> primes, int leftIdx, int rightIdx) {
+        byte cached = table[leftIdx][rightIdx];
+        if (cached == 0) {
+            boolean result = IntMath.isPrime(concat(primes.get(leftIdx), primes.get(rightIdx)));
+            table[leftIdx][rightIdx] = result ? (byte) 1 : -1;
+            return result;
+        } else {
+            return cached == (byte) 1;
+        }
     }
 
     private static int concat(int a, int b) {
