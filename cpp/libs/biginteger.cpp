478b24822956a9468f5741f06d75b5543c10a199
Kevin Zhang <kevkzhang@gmail.com>
2017-05-01 13:45:06 -0700

    Improve BigInteger.to_string()

--- cpp/libs/biginteger.cpp
+++ cpp/libs/biginteger.cpp
@@ -2,8 +2,8 @@
 
 using namespace std;
 
-inline unsigned int highest_order(unsigned int n) {
-    unsigned int mask = 1;
+inline uint32_t highest_order(uint32_t n) {
+    uint32_t mask = 1;
     while (n >>= 1) {
         mask <<= 1;
     }
@@ -11,29 +11,29 @@ inline unsigned int highest_order(unsigned int n) {
 }
 
 class BigInteger {
-    static const unsigned long BASE = 1L << 32;
+    static const uint64_t BASE = 1L << 32;
     private:
         // little endian, in increasing order of bits
-        vector<unsigned int> data;
-        void _add(vector<unsigned int>& number, const vector<unsigned int>& by, unsigned long base) {
-            unsigned int carry = 0;
+        vector<uint32_t> data;
+        void _add(vector<uint32_t>& number, const vector<uint32_t>& by, unsigned long base) {
+            uint32_t carry = 0;
             int common_length = min(number.size(), by.size());
             // add common bits
             for (int i = 0; i < common_length; i++) {
                 unsigned long result = ((unsigned long)number[i]) + by[i] + carry;
-                number[i] = (unsigned int) (result % base);
+                number[i] = (uint32_t) (result % base);
                 carry = result / base;
             }
             // my number is bigger
             for (int i = common_length; carry && i < number.size(); i++) {
                 unsigned long result = ((unsigned long)number[i]) + carry;
-                number[i] = (unsigned int) (result % base);
+                number[i] = (uint32_t) (result % base);
                 carry = result / base;
             }
             // their number is bigger
             for (int i = common_length; i < by.size(); i++) {
                 unsigned long result = ((unsigned long)by[i]) + carry;
-                number.push_back((unsigned int) (result % base));
+                number.push_back((uint32_t) (result % base));
                 carry = result / base;
             }
             // finish adding carry amounts
@@ -42,11 +42,12 @@ class BigInteger {
                 carry /= base;
             }
         }
-        void _add(vector<unsigned int>& number, unsigned int by, unsigned long base) {
-            unsigned int carry = by;
+        // TODO: unsafe, careful to not overflow the uint64_t
+        void _add(vector<uint32_t>& number, uint64_t by, unsigned long base) {
+            uint32_t carry = by;
             for (int i = 0; i < number.size(); i++) {
                 unsigned long result = ((unsigned long)number[i]) + carry;
-                number[i] = (unsigned int) (result % base);
+                number[i] = (uint32_t) (result % base);
                 carry = result / base;
             }
             while (carry) {
@@ -54,11 +55,12 @@ class BigInteger {
                 carry /= base;
             }
         }
-        void _times(vector<unsigned int>& number, unsigned int by, unsigned long base) {
-            unsigned int carry = 0;
+        // TODO: unsafe, careful to not overflow the uint64_t
+        void _times(vector<uint32_t>& number, uint64_t by, unsigned long base) {
+            uint32_t carry = 0;
             for (int i = 0; i < number.size(); i++) {
                 unsigned long result = ((unsigned long)number[i]) * by + carry;
-                number[i] = (unsigned int) (result % base);
+                number[i] = (uint32_t) (result % base);
                 carry = result / base;
             }
             while (carry) {
@@ -66,13 +68,13 @@ class BigInteger {
                 carry /= base;
             }
         }
-        static vector<unsigned int> zero() {
-            vector<unsigned int> number;
+        static vector<uint32_t> zero() {
+            vector<uint32_t> number;
             number.push_back(0);
             return number;
         }
-        static vector<unsigned int> one() {
-            vector<unsigned int> number;
+        static vector<uint32_t> one() {
+            vector<uint32_t> number;
             number.push_back(1);
             return number;
         }
@@ -86,7 +88,7 @@ class BigInteger {
         BigInteger(const BigInteger& big) {
             data = big.data;
         }
-        BigInteger(const vector<unsigned int>& vec) {
+        BigInteger(const vector<uint32_t>& vec) {
             data = vec;
         }
         BigInteger(const long number) : BigInteger(std::to_string(number)) {}
@@ -97,15 +99,15 @@ class BigInteger {
                 _add(data, number[i] - '0', BASE);
             }
         }
-        void times(unsigned int by) {
+        void times(uint32_t by) {
             _times(this->data, by, BASE);
         }
         void add(const BigInteger& big) {
             _add(this->data, big.data, BASE);
         }
         int compare(const BigInteger& big) {
-            vector<unsigned int> this_data = this->data;
-            vector<unsigned int> that_data = big.data;
+            vector<uint32_t> this_data = this->data;
+            vector<uint32_t> that_data = big.data;
             if (this_data.size() > that_data.size()) {
                 return 1;
             } else if (this_data.size() < that_data.size()) {
@@ -123,27 +125,10 @@ class BigInteger {
         }
         string to_string() {
             string str;
-            vector<unsigned int> base_10 = zero();
-            unsigned int highest_block = data.back();
-            unsigned int initial_mask = highest_order(highest_block);
-            while (initial_mask > 0) {
-                unsigned int to_add = initial_mask & highest_block;
-                _times(base_10, 2, 10);
-                if (to_add) {
-                    _add(base_10, 1, 10);
-                }
-                initial_mask >>= 1;
-            }
-            for (int idx = data.size() - 2; idx >= 0; idx--) {
-                unsigned int mask = 1 << 31;
-                while (mask > 0) {
-                    unsigned int to_add = mask & data[idx];
-                    _times(base_10, 2, 10);
-                    if (to_add) {
-                        _add(base_10, 1, 10);
-                    }
-                    mask >>= 1;
-                }
+            vector<uint32_t> base_10 = zero();
+            for (int idx = data.size() - 1; idx >= 0; idx--) {
+                _times(base_10, BASE, 10);
+                _add(base_10, data[idx], 10);
             }
             for (int idx = base_10.size() - 1; idx >= 0; idx--) {
                 str += std::to_string(base_10[idx]);
